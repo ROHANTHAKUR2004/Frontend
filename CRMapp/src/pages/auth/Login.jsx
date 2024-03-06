f67b0b531ad4746bde1d7e3418a66c3b3a53e903
rohan <rohanthakur89768@gmail.com>
2024-03-06 18:31:39 +0530

    added login using tailwind

---
+++ CRMapp/src/pages/auth/Login.jsx
@@ -0,0 +1,20 @@
+function Login(){
+
+    return(
+        <div className="flex justify-center items-center h-[90vh] ">
+        <div className="card w-96 bg-primary text-primary-content">
+         <div className="card-body">
+           <h2 className="card-title flex justify-center mb-3 items-center text-3xl text-black">Login</h2>
+           <input type="text" placeholder="User id.." className="input text-black input-bordered bg-white input-primary w-full max-w-xs" />
+           <input type="password" placeholder="Password.." className="input text-black input-bordered bg-white input-primary w-full max-w-xs" />
+           <div className="card-actions justify-end ">
+            <button className="btn mt-4 btn-warning w-full font-bold text-xl">Submit</button>
+          </div>
+        </div>
+        </div>
+        </div>
+    )
+
+}
+
+export default Login;
