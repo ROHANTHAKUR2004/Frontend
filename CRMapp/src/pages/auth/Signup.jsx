09634e712e78bcd2a35c0ac295b8764a2c0c9075
rohan <rohanthakur89768@gmail.com>
2024-03-24 09:44:58 +0530

    added axios instance

--- CRMapp/src/pages/auth/Signup.jsx
+++ CRMapp/src/pages/auth/Signup.jsx
@@ -4,7 +4,7 @@ function Signup(){
         <div className="flex justify-center items-center h-[90vh] ">
         <div className="card w-96 bg-primary text-primary-content">
          <div className="card-body">
-           <h2 className="card-title flex justify-center mb-3 items-center text-3xl text-black">Signup</h2>
+           <h2 className="card-title flex justify-center mb-3 items-center text-3xl text-black">SignUp</h2>
            <input type="text" placeholder="User id.." className="input text-black input-bordered bg-white input-primary w-full max-w-xs" />
            <input type="email" placeholder="Email.." className="input text-black input-bordered bg-white input-primary w-full max-w-xs" />
            <input type="password" placeholder="Password.." className="input text-black input-bordered bg-white input-primary w-full max-w-xs" />
