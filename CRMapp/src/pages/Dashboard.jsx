4495a1fb7f9eb3e83642b49aed0a048817033bcf
rohan <rohanthakur89768@gmail.com>
2024-03-26 16:52:47 +0530

    added react data table

--- CRMapp/src/pages/Dashboard.jsx
+++ CRMapp/src/pages/Dashboard.jsx
@@ -1,4 +1,6 @@
+import DataTable from 'react-data-table-component';
 import { FaCloudDownloadAlt } from "react-icons/fa";
+import { usePDF } from 'react-to-pdf';
 
 import HomeLayout from "../../src/Layouts/HomeLayouts";
 import useTickets from "../hooks/useTickets";
@@ -7,50 +9,68 @@ import useTickets from "../hooks/useTickets";
 export default function Dasboard(){
 
     const [ticketState] = useTickets();
+    const { toPDF , targetRef}  = usePDF({filename : 'page.pdf'})
+
+    const ExpandedComponent = ({ data }) => <pre>{JSON.stringify(data, null, 2)}</pre>;
+    const columns = [
+        {
+            name: 'Tickect Id',
+            selector: row => row._id,
+            reorder : true,
+        },
+        {
+            name: 'Title',
+            selector: row => row.title,
+            reorder : true,
+        },
+        {
+            name: 'Description',
+            selector: row => row.description,
+            reorder : true,
+        },
+        {
+            name: 'Reporter',
+            selector: row => row.assignedTo,
+            reorder : true,
+        },
+        {
+            name: 'Priority',
+            selector: row => row.ticketPriority,
+            sortable : true,
+            reorder : true,
+        },
+        {
+            name: 'Assignee',
+            selector: row => row.assignee,
+            reorder : true,
+        }, 
+        {
+            name: 'Status',
+            selector: row => row.status,
+            sortable : true,
+            reorder : true,
+        },
+    ];
+
     
     return(
         <HomeLayout>
             <div className="min-h-[90vh] ml-20 flex flex-col items-center justify-center gap-2" >
               
-                <div className="bg-yellow-500 w-full text-black text-center text-3xl py-4 font-bold hover:bg-yellow-300 transition-all ease-in-out duration-300">
-                     Tickets Records <FaCloudDownloadAlt  className="inline "/>
+                <div className="bg-yellow-500 w-full text-black text-center text-3xl py-4 font-bold hover:bg-yellow-300 transition-all ease-in-out duration-300 max-w-[85vh]">
+                     Tickets Records <FaCloudDownloadAlt onClick={() => toPDF()} className="inline "/>
                 </div>
-
-                {/* table */}
-                <div className="flex flex-col w-full" >
-                    {/* title row */}
-                    <div className="flex justify-between font-bold items-center gap-3 px-2 py-2 bg-purple-600">
-                        <div className="table-title basis-[9%] justify-start">Ticket Id</div>
-                        <div className="table-title basis-[14%]">Title</div>
-                        <div className="table-title basis-[20%]">Description</div>
-                        <div className="table-title basis-[20%]">Reporter</div>
-                        <div className="table-title basis-[4%]">Priortiy</div>
-                        <div className="table-title basis-[21%]">Assignee</div>
-                        <div className="table-title basis-[12%]">Status</div>
-                    </div>
-                {/* ticket details */}
                  
-               {ticketState && ticketState.ticketList.map(ticket =>{
-                        return(
-                            <div key={ticket._id} className=" my-2 py-2 font-normal  text-sm flex justify-between items-center gap-3 px-2 text-black  bg-gray-100 hover:bg-gray-400 transition-all ease-in-out duration-300">
-                            <div className="table-title basis-[9%] justify-start">{ticket._id.substring(0,5)+"."}</div>
-                            <div className="table-title basis-[14%]">{ticket.title}</div>
-                            <div className="table-title basis-[20%]">{ticket.description}</div>
-                            <div className="table-title basis-[20%]">{ticket.assignee}</div>
-                            <div className="table-title basis-[4%]">{ticket.ticketPriority}</div>
-                            <div className="table-title basis-[21%]">{ticket.assignedTo}</div>
-                            <div className="table-title basis-[12%]">{ticket.status}</div>
-                        </div>
-                        );
-               })}
+               <div ref={targetRef}>
+                {ticketState && <DataTable
+                columns={columns}
+                data={ticketState.ticketList}
+                expandableRows
+			    expandableRowsComponent={ExpandedComponent}
                
-
-                </div>
-
-
-
+                />}
+              </div>
             </div>
-
         </HomeLayout>
     );
 }
