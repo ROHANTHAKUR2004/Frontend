95de18af592b5449da0eecf9caa8528b2e53ed9b
rohan <rohanthakur89768@gmail.com>
2024-03-27 11:14:52 +0530

    added pie chart

--- CRMapp/src/pages/home/Home.jsx
+++ CRMapp/src/pages/home/Home.jsx
@@ -4,31 +4,148 @@ import { IoCheckmarkDoneCircleSharp } from "react-icons/io5";
 import { MdPending } from "react-icons/md";
 import { MdCancel } from "react-icons/md";
 import { TbProgressBolt } from "react-icons/tb";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend , Title} from "chart.js";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend , Title, CategoryScale, LinearScale, PointElement , LineElement} from "chart.js";
 import Cart from "../../components/Cart";
 import useTickets from "../../hooks/useTickets";
 import HomeLayout from "../../Layouts/HomeLayouts";
-import { Pie } from 'react-chartjs-2';
+import { Pie, Line } from 'react-chartjs-2';
+import { useEffect, useState } from "react";
 
 
-ChartJS.register( ArcElement, Tooltip, Legend , Title);
+ChartJS.register( ArcElement, Tooltip, Legend , Title, CategoryScale,LinearScale,LineElement,PointElement);
 
 export default function Home(){
 
     
     const [ticketState]  = useTickets();
+    const [ticketschartdata , setticketschartdata] = useState({
+        opentickets : [],
+        closedtickets : [], 
+        resolvedtickets : [],
+        inprogresstickets :[],
+        onholdtickets :[]
+    });
 
     const piechartdata = {
            labels : Object.keys(ticketState.ticketDistribution),
            fontColor : "white",
            datasets : [
             {
-               data : Object.values(ticketState.ticketDistribution) ,
+              label : "Tickets data",
+              data : Object.values(ticketState.ticketDistribution) ,
                backgroundColor :["yellow" , "red", "green" , "gray", "orange" ],
                borderColor : ["yellow" , "red","green" , "gray", "orange" ]   
             }
            ]
+    };
+
+   
+
+    const linechartdata = {
+      labels : Object.keys(ticketschartdata.opentickets),
+      fontColor : "white",
+      datasets : [
+       {
+        label : "Open Tickets data",
+        data : Object.values(ticketschartdata.opentickets) ,
+        borderColor : 'rgb(255, 99,132)',
+        backgroundColor : 'rgba(255, 99 132 0.5)',
+       }, {
+        label : "Cancelled Tickets data",
+        data : Object.values(ticketschartdata.closedtickets) ,
+        borderColor : 'rgb(32, 40,132)',
+        backgroundColor : 'rgba(255, 99 132 0.5)',
+       },
+       {
+        label : "Resolved Tickets data",
+        data : Object.values(ticketschartdata.resolvedtickets) ,
+        borderColor : 'rgb(70, 35,32)',
+        backgroundColor : 'rgba(255, 99 132 0.5)',
+       },{
+        label : "InProgress Tickets data",
+        data : Object.values(ticketschartdata.inprogresstickets) ,
+        borderColor : 'rgb(100, 80,32)',
+        backgroundColor : 'rgba(255, 99 132 0.5)',
+       },{
+        label : "OnHold Tickets data",
+        data : Object.values(ticketschartdata.onholdtickets) ,
+        borderColor : 'rgb(200,12,32)',
+        backgroundColor : 'rgba(255, 99 132 0.5)',
+       }
+      ]
+         
+    };
+
+    function processtickets(){
+
+  
+    const currentDate = new Date();
+    const tenthDayfromtoday = new Date();
+    tenthDayfromtoday.setDate(currentDate.getDate() - 10);
+    
+  
+      if(ticketState.ticketList.length > 0){
+        let openticketdata = {};
+        let closedticketdata = {};
+        let resolvedticketdata = {};
+        let inprogressticketdata = {};
+        let onholdticketdata = {}
+      
+
+        for(let i=0;i<10;i++){
+          const dateobject = new Date();
+          dateobject.setDate(currentDate.getDate() - i)
+          
+           openticketdata[dateobject.toLocaleDateString('en-CA').split("/").reverse().join("-")] = 0;
+           closedticketdata[dateobject.toLocaleDateString('en-CA').split("/").reverse().join("-")] = 0;
+           resolvedticketdata[dateobject.toLocaleDateString('en-CA').split("/").reverse().join("-")] = 0;
+           inprogressticketdata[dateobject.toLocaleDateString('en-CA').split("/").reverse().join("-")] = 0;
+           onholdticketdata[dateobject.toLocaleDateString('en-CA').split("/").reverse().join("-")] = 0;
+
+           //console.log(openticketdata);
+        }
+
+        ticketState.ticketList.forEach(ticket =>{
+         const date = ticket.createdAt.split("T")[0];
+         const TicketDate = new Date(ticket.createdAt);
+         
+         if(ticket.status == "open" &&  TicketDate >= tenthDayfromtoday ){
+           openticketdata[date] =  openticketdata[date] + 1;
+         }
+         if(ticket.status == "cancelled" &&  TicketDate >= tenthDayfromtoday ){
+          closedticketdata[date] =  closedticketdata[date] + 1;
+        }
+        if(ticket.status == "resolved" &&  TicketDate >= tenthDayfromtoday ){
+          resolvedticketdata[date] =  resolvedticketdata[date] + 1;
+        }
+        if(ticket.status == "inProgress" &&  TicketDate >= tenthDayfromtoday ){
+          inprogressticketdata[date] =  inprogressticketdata[date] + 1;
+        }
+        if(ticket.status == "onHold" &&  TicketDate >= tenthDayfromtoday ){
+          onholdticketdata[date] =  onholdticketdata[date] + 1;
+        }
+        });
+       // console.log("next")
+       // console.log(openticketdata)
+       setticketschartdata({
+         opentickets : openticketdata,
+         closedtickets : closedticketdata,
+         resolvedtickets : resolvedticketdata,
+         inprogresstickets : inprogressticketdata,
+         onholdtickets : onholdticketdata
+       })
+       
+       //setclosedtickets(closedticketdata);
+     }
     }
+    
+
+    useEffect(()=>{
+       processtickets();
+    },[ticketState.ticketList])
+
+
+
 
     return(
         <HomeLayout>
@@ -110,8 +227,16 @@ export default function Home(){
           data={piechartdata}
           />
         </div>
-
         </div>
+
+        <div className="flex justify-center items-center mt-10 gap-10">
+        <div className="w-[40rem] h-[40rem]">
+           <Line
+           data={linechartdata}
+            />
+            </div>
+        </div>
+        
         </HomeLayout>
 
         );
